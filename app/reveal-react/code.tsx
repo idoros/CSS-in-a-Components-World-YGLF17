d241c730825748cd710abcedc0f75ab7aeabbff8
Idoro <idoro@wix.com>
2017-10-16 15:31:34 +0300

    add MarkCode component

--- app/reveal-react/code.tsx
+++ app/reveal-react/code.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
+import * as deindent from 'deindent';
 
 type CodeProps = {
     children:React.ReactNode;
-    lang?:string;
+    lang?:'css'|'javascript'|string;
     noEscape?:boolean;
     trim?:boolean;
 };
@@ -14,3 +15,16 @@ export const Code = ({children, lang, noEscape, trim}:CodeProps) => {
         </pre>
     );
 }
+
+type MarkCodeProps = {
+    children:string;
+    lang?:string;
+    markSymbol?:string;
+    markMapping?:{[s:string]:string};
+};
+export const MarkCode = ({children, lang, markSymbol = '%', markMapping = {}}:MarkCodeProps) => {
+    const code = deindent(children).trim();
+    const markSplit = code.split(new RegExp(`${markSymbol}([^${markSymbol}]+)${markSymbol}`));
+    const codeTarget = markSplit.map((snip, index) => index % 2 ? <mark data-name={markMapping[snip]}>{snip}</mark> : snip);
+    return <Code lang={lang} trim={true} noEscape={codeTarget.length>1}>{codeTarget}</Code>
+};
